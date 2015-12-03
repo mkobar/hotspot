fdc9d34fffd316d5b40dc6106bc9b40d2d695d68
Clifford Fajardo <fajardoclifford@gmail.com>
2015-12-03 10:29:54 -0800

    added simple test-spec to check if Karma is working

---
+++ test/controllers.js
@@ -0,0 +1,13 @@
+
+/*
+  Testing block for loading posts on homeview
+*/
+describe('Homepage', function(){
+  describe('', function(){
+    it('should return the sum of two numbers', function(){
+      expect(add(1,2)).toBe(3);
+    })
+
+  })
+
+});
