dbf811638b60ca5dc119f7d03157fa571205be74
Clifford Fajardo <fajardoclifford@gmail.com>
2015-12-05 11:36:47 -0800

    Posts & comments now have unique id's

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -72,7 +72,6 @@ angular.module('app.controllers', [])
 
 
     $scope.addComment = function(){
-      console.log('add comment worked??')
       if($scope.message === "" || $scope.message === undefined){return;}
       //clear input fields after submit
       $scope.message = '';
