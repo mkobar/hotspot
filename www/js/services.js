77282d54b04b33bb381591ba5e6b5fb5dafebbaa
Clifford Fajardo <fajardoclifford@gmail.com>
2015-12-08 14:26:50 -0800

    starting to work on comments

--- www/js/services.js
+++ www/js/services.js
@@ -25,14 +25,19 @@ angular.module('app.services', [])
         console.log('response.data', response.data);
         return response.data;
       });
+    };
 
+    o.upvotePost = function(){
+      return $http({method: 'GET',url: '/posts/' + id})
+      .then(function(response){
+        console.log('response.data', response.data);
+        return response.data;
+      });
     };
 
 
-  return {
-    getPosts : getPosts,
-    posts : posts
-  };
+
+  return o;
 }])
 
 .factory('CameraFactory', ['$cordovaCamera','$http', function($cordovaCamera, $http){
@@ -73,20 +78,22 @@ angular.module('app.services', [])
 
 }])
 
-.factory('LocationFactory', ['$cordovaGeolocation', function($cordovaGeolocation){
 
-  var getPosition = function(){
-    var options = {
-      setTimeout : 10000,
-      enableHighAccuracy : true
+
+.factory('LocationFactory', [
+  '$cordovaGeolocation',
+   function($cordovaGeolocation){
+
+    var getPosition = function(){
+      var options = {
+        setTimeout : 10000,
+        enableHighAccuracy : true
+      };
+      return $cordovaGeolocation.getCurrentPosition(options);
     };
 
-    return $cordovaGeolocation.getCurrentPosition(options);
-  };
-
-
-  return {
-    getPosition : getPosition
-  };
+    return {
+      getPosition : getPosition
+    };
 
 }]);
