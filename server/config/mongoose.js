cb8dd51b5834ac70b3abbfea041b18f36617301a
starcraft168 <ericeng168@gmail.com>
2015-12-08 14:37:29 -0800

    editing

--- server/config/mongoose.js
+++ server/config/mongoose.js
@@ -4,6 +4,7 @@ var path = require('path');
 
 //connect to the hotspot database
 
+
 // mongoose.connect('mongodb://localhost/hotspot');
 mongoose.connect('mongodb://hotspot:123@ds027719.mongolab.com:27719/heroku_0vjk4zkx');
 
