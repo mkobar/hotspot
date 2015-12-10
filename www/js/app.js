80fa94a8c92346041a77b2e01794218667ee7290
Gabriel Matias <gabriel.2m.deleon@gmail.com>
2015-12-10 14:52:59 -0800

    chore: repairing git issues

--- www/js/app.js
+++ www/js/app.js
@@ -20,7 +20,7 @@ angular.module('app', [
     // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
     // for form inputs)
     if(window.cordova && window.cordova.plugins.Keyboard) {
-      cordova.plugins.Keyboard.hideKeyboardAccessoryBar(false)
+      cordova.plugins.Keyboard.hideKeyboardAccessoryBar(false);
     }
     if(window.StatusBar) {
       // org.apache.cordova.statusbar required
