7e49aa2442d82d14a78b432b9d7c9f931fa8f924
Clifford Fajardo <fajardoclifford@gmail.com>
2015-12-28 15:41:10 -0800

    new file location

--- www/app/app.js
+++ www/app/app.js
@@ -7,12 +7,8 @@
 // 'starter.controllers' is found in controllers.js
 angular.module('app', [
   'ionic',
-  'app.controllers',
   'app.routes',
-  'app.services',
-  'app.directives',
   'ngCordova'
-  //next dependencies go here...
 ])
 
 .run(function($ionicPlatform) {
@@ -23,7 +19,6 @@ angular.module('app', [
       cordova.plugins.Keyboard.hideKeyboardAccessoryBar(false);
     }
     if(window.StatusBar) {
-      // org.apache.cordova.statusbar required
       StatusBar.styleDefault();
       navigator.splashscreen.hide();
     }
@@ -34,5 +29,3 @@ angular.module('app', [
   // url: 'https://gentle-spire-1503.herokuapp.com'
   url: 'http://localhost:3000'
 });
-
-
