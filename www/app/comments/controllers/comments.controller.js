0f86282379433e309c9401fc2b0ae1c2afb97caa
Clifford Fajardo <fajardoclifford@gmail.com>
2016-01-15 08:31:05 -0800

    code style update

--- www/app/comments/controllers/comments.controller.js
+++ www/app/comments/controllers/comments.controller.js
@@ -2,9 +2,7 @@ angular.module('app.CommentsController', [])
   //in order to get the route parameters from the url (e.g, posts/{id}) we need to inject this $stateParams
   .controller('CommentsController', ['$scope', '$stateParams', 'CommentsFactory', 'singlePost', function($scope, $stateParams, CommentsFactory, singlePost) {
     $scope.post = singlePost;
-    $scope.comment = {
-      input: ""
-    };
+    $scope.comment = { input: ""};
     $scope.addComment = function() {
       if(!$scope.comment.input) {return;}
       CommentsFactory.addComment(singlePost._id, $scope.comment.input)
